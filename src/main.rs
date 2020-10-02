485f02f09623bc0303a5540c24918303626bddb1
Radovan Bast <bast@users.noreply.github.com>
2020-10-02 22:20:38 +0200

    show how to relative import to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+mod riddler;
+
 fn main() {
     println!("Na na na na na na na na Batman!");
 
+    // using a library function
     let quote = gotham::batman_quote();
     println!("Batman says: {}", quote);
+
+    // this function is not part of the library
+    let quote = riddler::difficult_riddle();
+    println!("Riddler says: {}", quote);
 }
